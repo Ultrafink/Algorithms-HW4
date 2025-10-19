505a607978752c95c0e176fa25a9219bf40f331f
Ultrafink <finglar@gmail.com>
2025-10-19 12:30:44 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+function findKthElement(arr1, arr2, k) {
+  let i = 0, j = 0, count = 0;
+  let element;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      element = arr1[i];
+      i++;
+    } else {
+      element = arr2[j];
+      j++;
+    }
+    count++;
+    if (count === k) return element;
+  }
+
+  // Если элементы остались только в одном массиве
+  while (i < arr1.length) {
+    element = arr1[i];
+    i++;
+    count++;
+    if (count === k) return element;
+  }
+
+  while (j < arr2.length) {
+    element = arr2[j];
+    j++;
+    count++;
+    if (count === k) return element;
+  }
+}
+
+// Проверим
+const arr1 = [100, 112, 256, 349, 770];
+const arr2 = [72, 86, 113, 119, 265, 445, 892];
+const k = 7;
+
+console.log(`7-й элемент: ${findKthElement(arr1, arr2, k)}`);
